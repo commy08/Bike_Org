a70a1631ab5209a66ceecd1a1f9c3c132d18866d
commy08 <noppawongsurintham@gmail.com>
2018-10-04 21:57:28 +0700

    laravel

---
+++ laravel_backend/database/migrations/2018_10_04_142240_create_users_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('line_id');
+            $table->text('line_pic');
+            $table->text('line_token');
+            $table->string('type',1);
+            $table->string('firstname',100);
+            $table->string('lastname',100);
+            $table->enum('sex',['male','female']);
+            $table->text('address');
+            $table->date('birthday');
+            $table->text('picID');
+            $table->text('picORG');
+            $table->text('OrgName');
+            $table->text('tradeNum');
+            $table->string('tel',10);
+            $table->string('status',1);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
