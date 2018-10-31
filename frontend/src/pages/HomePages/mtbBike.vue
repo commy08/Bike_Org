6b23c5e561bcca5c4b28887e6ee56de1ce03b89e
Teerapat19 <31680586+Teerapat19@users.noreply.github.com>
2018-10-31 23:51:21 +0700

    UpdateForm

--- frontend/src/pages/HomePages/mtbBike.vue
+++ frontend/src/pages/HomePages/mtbBike.vue
@@ -38,70 +38,6 @@
                 </v-flex>
             </v-layout>
         </v-container>
-                     <!-- Modal -->
-                        <v-dialog v-model="dialog" width="1000px">
-                            <v-card>
-                                <v-card-title
-                                class="dark lighten-4 py-4 title"
-                                >
-                                Create contact
-                                </v-card-title>
-                                <v-container grid-list-sm class="pa-4">
-                                <v-layout row wrap>
-                                    <v-flex xs12 align-center justify-space-between>
-                                    <v-layout align-center>
-                                        <v-avatar size="40px" class="mr-3">
-                                        <img
-                                            src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png"
-                                            alt=""
-                                        >
-                                        </v-avatar>
-                                        <v-text-field
-                                        placeholder="Name"
-                                        ></v-text-field>
-                                    </v-layout>
-                                    </v-flex>
-                                    <v-flex xs6>
-                                    <v-text-field
-                                        prepend-icon="business"
-                                        placeholder="Company"
-                                    ></v-text-field>
-                                    </v-flex>
-                                    <v-flex xs6>
-                                    <v-text-field
-                                        placeholder="Job title"
-                                    ></v-text-field>
-                                    </v-flex>
-                                    <v-flex xs12>
-                                    <v-text-field
-                                        prepend-icon="mail"
-                                        placeholder="Email"
-                                    ></v-text-field>
-                                    </v-flex>
-                                    <v-flex xs12>
-                                    <v-text-field
-                                        type="tel"
-                                        prepend-icon="phone"
-                                        placeholder="(000) 000 - 0000"
-                                        mask="phone"
-                                    ></v-text-field>
-                                    </v-flex>
-                                    <v-flex xs12>
-                                    <v-text-field
-                                        prepend-icon="notes"
-                                        placeholder="Notes"
-                                    ></v-text-field>
-                                    </v-flex>
-                                </v-layout>
-                                </v-container>
-                                <v-card-actions>
-                                <v-spacer></v-spacer>
-                                <v-btn flat color="primary" @click="dialog = false">Cancel</v-btn>
-                                <v-btn flat @click="dialog = false">Save</v-btn>
-                                </v-card-actions>
-                            </v-card>
-                        </v-dialog>
-                    <!-- End Modal -->
     </v-app>
 </template>
 
@@ -109,7 +45,7 @@
   export default {
     data: () => ({
         dialog: false,
-        show: [],
+        show: false,
     }),
   };
 </script>
