d13f10dc854668fd87bce05ec4303310023ef493
commy08 <noppawongsurintham@gmail.com>
2018-12-09 19:56:34 +0700

    update address

---
+++ frontend/src/pages/HomePages/city.vue
@@ -0,0 +1,104 @@
+<!----------Make By YourName---------------->
+ <template>
+<v-content>
+
+     <v-select
+     v-model="form.city"
+     @change="getAmp"
+          :items="city"
+          item-value="id"
+          item-text="province_name"
+          box
+          label="Select City"
+        ></v-select>
+
+   <v-select 
+     v-model="form.amp" 
+          :items="amp"
+          item-value="id"
+          item-text="amphur_name"
+          box
+          label="Select Amp"
+        ></v-select>
+        
+<h2>CityId = {{form.city}}</h2>
+<h2>AmpId = {{form.amp}}</h2>
+
+ 
+    </v-content>
+</template>
+
+    <script>
+    import axios from '@/axios';
+
+export default {
+    name: 'Root',
+    /*-------------------------Load Component---------------------------------------*/
+    components: {
+
+    },
+  /*-------------------------Set Component---------------------------------------*/
+props:{
+
+},
+    /*-------------------------DataVarible---------------------------------------*/
+    data() {
+    return {
+        
+        city:[],
+        amp:[],
+
+        ampSelect:{},
+
+
+        form:{
+            city:'',
+            amp:'',
+        }
+        };
+    }, 
+    /*-------------------------Run Methods when Start this Page------------------------------------------*/
+     async mounted() {
+    /**** Call loading methods*/
+            this.load(); 
+    },
+    /*-------------------------Run Methods when Start Routed------------------------------------------*/
+     async beforeRouteEnter(to, from, next) { 
+        next()
+    },
+    /*-------------------------Vuex Methods and Couputed Methods------------------------------------------*/
+    computed:{
+
+},
+    /*-------------------------Methods------------------------------------------*/
+methods:{
+
+    getCity:async function(){
+
+       let u =    await axios.get("http://192.168.1.15:8082/api/getCity").then((r) => {
+                this.city =  r.data;
+            }).catch((e) => {
+                  alert('Error City');
+            });
+
+
+    },
+    getAmp:async function(){
+       
+            let u =    await axios.get("http://192.168.1.15:8082/api/getAmp/"+this.form.city).then((r) => {
+                this.amp =  r.data;
+            }).catch((e) => {
+                  alert('Error amp');
+            }); 
+         
+
+    },
+
+
+    /******* Methods default run ******/
+    load:async function(){
+            this.getCity();  
+        }
+},
+    }
+</script>
